3804c479dbad8e2f5d2262bb8ef05b9893a8c3aa
areyes26 <areyesabarca@udesa.edu.ar>
2021-05-27 21:03:11 -0300

    Update App.js

--- ProyectoFinal/App.js
+++ ProyectoFinal/App.js
@@ -11,28 +11,39 @@ export default class App extends Component {
 
   render(){
     return (
-    <View style={{flex:1, backgroundColor: "#30444E"}}>  
+    <View style={{flex:1, backgroundColor: "#22343C"}}>  
      <View style={{backgroundColor: "#22343C", height:"4%", width: "100%",}}></View>
 
      <View style={{backgroundColor: "blue", height:"10%",flexDirection:"row", }}>
         <View style={{width:"9.5%", marginLeft:"7.7%", height:"100%",justifyContent:"center"}}>
-          <Button color="#3DD598" title="+" style={{height:"6%"}}></Button>
+          <Button color="#3DD598" title="+" style={{height:"6%", }}></Button>
         </View>
         <View style={{width:"49%",marginLeft:"11.3%", height:"100%",justifyContent:"center" }}>
-          <Button color="#3DD598" title="buscador" style={{height:"6%"}}></Button>
+          <Button color="#3DD598" title="buscador" style={{height:"6%", }}></Button>
         </View>
         <View style={{width:"13%",marginLeft:"4%", height:"100%" ,justifyContent:"center"}}>
-          <Button color="#3DD598" title="F" style={{height:"6%"}}></Button>
+          <Button color="#3DD598" title="F" style={{height:"6%", }}></Button>
         </View>  
      </View>
 
-     <View style={{backgroundColor: "yellow", height:"8%", width: "100%",}}>
-       
+     <View style={{backgroundColor: "yellow", height:"8%",flexDirection:"row", justifyContent:"space-evenly"}}>
+        <View style={{ height:"100%",justifyContent:"center"}}>
+          <Button color="#3DD598" title="All" style={{height:"6%", }}></Button>
+        </View>
+        <View style={{ height:"100%",justifyContent:"center" }}>
+          <Button color="#3DD598" title="Edad" style={{height:"6%", }}></Button>
+        </View>
+        <View style={{ height:"100%" ,justifyContent:"center"}}>
+          <Button color="#3DD598" title="A to Z" style={{height:"6%", }}></Button>
+        </View>
+        <View style={{ height:"100%" ,justifyContent:"center"}}>
+          <Button color="#3DD598" title="Z to A" style={{height:"6%", }}></Button>
+        </View>
      </View>
 
-     <View style={{backgroundColor: "#22343C", height:"66%", width: "100%",}}></View>
+     <View style={{backgroundColor: "#22343C", height:"66.5%", width: "100%",}}></View>
 
-     <View style={{backgroundColor: "#30444E", height:"12%", width: "100%", flexDirection: "row", justifyContent: "space-evenly", borderRadius: "25px 25px 0px 0xpx"}}>
+     <View style={{backgroundColor: "#30444E", height:"11.5%", width: "100%", flexDirection: "row", justifyContent: "space-evenly", borderTopLeftRadius: "25px", borderTopRightRadius: "25px" }}>
        <Button color="#3DD598" title= "Hola" style={{width:"20%"}}></Button>
        <Button color="#3DD598" title={<Image style={{width: 66,height: 58}} source={require("./assets/Path.png")}/>} style={{width:"20%"}}></Button>
        <Button color="#3DD598" title= "Hola" style={{width:"%"}}></Button>
