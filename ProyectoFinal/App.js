6f99cd94b96535b8fa607c42e91d666d0cb88c49
areyes26 <areyesabarca@udesa.edu.ar>
2021-05-27 21:27:56 -0300

    Fix de CSS
    Agrupe el css para que se entienda mejor todo

--- ProyectoFinal/App.js
+++ ProyectoFinal/App.js
@@ -5,6 +5,7 @@ import{
   View,
   Button,
   Image,
+  StyleSheet,
 
 } from "react-native";
 export default class App extends Component {
@@ -12,38 +13,38 @@ export default class App extends Component {
   render(){
     return (
     <View style={{flex:1, backgroundColor: "#22343C"}}>  
-     <View style={{backgroundColor: "#22343C", height:"4%", width: "100%",}}></View>
+     <View style={style.generalBackground,{height:"4%", width: "100%",}}></View>
 
-     <View style={{backgroundColor: "blue", height:"10%",flexDirection:"row", }}>
-        <View style={{width:"9.5%", marginLeft:"7.7%", height:"100%",justifyContent:"center"}}>
-          <Button color="#3DD598" title="+" style={{height:"6%", }}></Button>
+     <View style={style.generalBackground,{height:"10%",flexDirection:"row", }}>
+        <View style={style.botonMas}>
+          <Button color="#3DD598" title="+" style={style.botonesGeneral}></Button>
         </View>
-        <View style={{width:"49%",marginLeft:"11.3%", height:"100%",justifyContent:"center" }}>
-          <Button color="#3DD598" title="buscador" style={{height:"6%", }}></Button>
+        <View style={style.buscador}>
+          <Button color="#3DD598" title="buscador" style={style.botonesGeneral}></Button>
         </View>
-        <View style={{width:"13%",marginLeft:"4%", height:"100%" ,justifyContent:"center"}}>
-          <Button color="#3DD598" title="F" style={{height:"6%", }}></Button>
+        <View style={style.filtros}>
+          <Button color="#3DD598" title="F" style={style.botonesGeneral}></Button>
         </View>  
      </View>
 
-     <View style={{backgroundColor: "yellow", height:"8%",flexDirection:"row", justifyContent:"space-evenly"}}>
-        <View style={{ height:"100%",justifyContent:"center"}}>
-          <Button color="#3DD598" title="All" style={{height:"6%", }}></Button>
+     <View style={style.generalBackground,{height:"8%",flexDirection:"row", justifyContent:"space-evenly"}}>
+        <View style={style.botonesCategorias}>
+          <Button color="#3DD598" title="All" style={style.botonesGeneral}></Button>
         </View>
-        <View style={{ height:"100%",justifyContent:"center" }}>
-          <Button color="#3DD598" title="Edad" style={{height:"6%", }}></Button>
+        <View style={style.botonesCategorias}>
+          <Button color="#3DD598" title="Edad" style={style.botonesGeneral}></Button>
         </View>
-        <View style={{ height:"100%" ,justifyContent:"center"}}>
-          <Button color="#3DD598" title="A to Z" style={{height:"6%", }}></Button>
+        <View style={style.botonesCategorias}>
+          <Button color="#3DD598" title="A to Z" style={style.botonesGeneral}></Button>
         </View>
-        <View style={{ height:"100%" ,justifyContent:"center"}}>
-          <Button color="#3DD598" title="Z to A" style={{height:"6%", }}></Button>
+        <View style={style.botonesCategorias}>
+          <Button color="#3DD598" title="Z to A" style={style.botonesGeneral}></Button>
         </View>
      </View>
 
-     <View style={{backgroundColor: "#22343C", height:"66.5%", width: "100%",}}></View>
+     <View style={{ height:"66.5%", width: "100%",}}></View>
 
-     <View style={{backgroundColor: "#30444E", height:"11.5%", width: "100%", flexDirection: "row", justifyContent: "space-evenly", borderTopLeftRadius: "25px", borderTopRightRadius: "25px" }}>
+     <View style={style.generalBackground,{backgroundColor: "#30444E", height:"11.5%", width: "100%", flexDirection: "row", justifyContent: "space-evenly", borderTopLeftRadius: "25px", borderTopRightRadius: "25px" }}>
        <Button color="#3DD598" title= "Hola" style={{width:"20%"}}></Button>
        <Button color="#3DD598" title={<Image style={{width: 66,height: 58}} source={require("./assets/Path.png")}/>} style={{width:"20%"}}></Button>
        <Button color="#3DD598" title= "Hola" style={{width:"%"}}></Button>
@@ -53,7 +54,35 @@ export default class App extends Component {
     </View>
     );
   }
-  }
-  
+  };
 
-;
+  const style = StyleSheet.create({
+  botonesCategorias:{
+    height:"100%",
+    justifyContent:"center",
+  },
+  botonMas:{
+    width:"9.5%",
+    marginLeft:"7.7%",
+    height:"100%",
+    justifyContent:"center"
+  },
+  buscador:{
+    width:"49%",
+    marginLeft:"11.3%",
+    height:"100%",
+    justifyContent:"center"
+  },
+  filtros:{
+    width:"13%",
+    marginLeft:"4%",
+    height:"100%",
+    justifyContent:"center"
+  },
+  botonesGeneral:{
+    height:"6%"
+  },
+  generalBackground:{
+    backgroundColor: "#22343C"
+  }
+  })
